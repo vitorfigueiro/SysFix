f878ae5542e33b56dd450a66bd69674f90168ef2
Vitor <figueiro22.22@gmail.com>
2026-09-04 17:29:11 -0400

    fix: atualiza TemplatResponse

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def startup_event():
 # ----------------------------------------------------
 @app.get("/", response_class=HTMLResponse)
 def index(request: Request):
-    return templates.TemplateResponse("index.html", context={"request": request})
+    return templates.TemplateResponse(request=request, name="index.html")
 
 
 # ----------------------------------------------------
